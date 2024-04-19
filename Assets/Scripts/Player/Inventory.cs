28a20a7b4210234dc73d558a50c264cdb9052294
Belxsi <130850279+Belxsi@users.noreply.github.com>
2024-04-19 16:04:29 +0300

    Старт инвентарь + меню

---
+++ Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Inventory : MonoBehaviour
+{
+    public Dictionary<string, Item> items = new();
+    void Start()
+    {
+        
+    }
+
+    public void Add(ItemObject io)
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+}
+[Serializable]
+public class Item
+{
+
+}
---
+++ Assets/Scripts/Player/ItemObject.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    public Item item;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
