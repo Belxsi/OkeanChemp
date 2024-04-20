1b11f053b548645e24f4271470f47643b96b953d
Belxsi <130850279+Belxsi@users.noreply.github.com>
2024-04-20 08:59:38 +0300

    PrefabLoader

--- Assets/Scripts/BaseFunc.cs
+++ Assets/Scripts/BaseFunc.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
 public class BaseFunc : MonoBehaviour
 {
     public static bool IsLoad;
-    Dictionary<string, GameObject> prefabs;
+    public static Dictionary<string, GameObject> prefabs = new();
+   
+    public static void AddPrefab(string nam)
+    {
+        prefabs.Add(nam, (GameObject)PrefabLoader.Load("Prefabs", nam));
+    }
     public static void AllLoadPrefab()
     {
         if (!IsLoad)
         {
             IsLoad = true;
-
+            AddPrefab("Item");
+            AddPrefab("ItemInterface");
         }
     }
 }
