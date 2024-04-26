f95a28ffe61cf9e922cf4bd3333165e322ccdbfd
Belxsi <130850279+Belxsi@users.noreply.github.com>
2024-04-26 18:44:02 +1000

    TriggerWin

---
+++ Assets/TimeController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        Time.timeScale = 1; 
+    }
+
+   
+}
